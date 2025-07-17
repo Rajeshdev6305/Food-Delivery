ba09b0bc08ae9018a6ccf6da2056e1650a6b2220
Rajeshdev6305 <tammisettyrajesh0@gmail.com>
2025-07-17 14:03:20 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-1wrr.onrender.com'
+export const url = "https://food-delivery-backend-1wrr.onrender.com"
